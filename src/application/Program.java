3721942cb27e3892bbf3590bdb99f76d1129a4c8
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-04-10 15:25:43 -0300

    extends, super e instanceof

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+		// UPCASTING
+
+		Account acc1 = bacc; // é possível atribuir uma propriedade "bacc" em uma conta "acc", pois trata-se
+								// de uma subclasse, ou seja, uma BusinessAccount
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+		// DOWNCASTING
+
+		BusinessAccount acc4 = (BusinessAccount) acc2; // desta forma, força-se a instanciação como businessaccount
+		acc4.loan(100.0);
+
+		// BusinessAccount acc5 = (BusinessAccount) acc3; -> dá erro, pois acc3 é
+		// SavingAccount
+		// para resolver, verifica-se se o acc3 foi instanciada (com intanceof) em
+		// Business Account:
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount) acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount) acc3;
+			acc5.deposit(200.0);
+			System.out.println("Update!");
+		}
+
+	}
+
+}
---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class BusinessAccount extends Account { // extends (puxa todos os dados da Account)
+
+	private Double loanLimit;
+
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance); // utiliza-se este construtor para puxar as informações da superclasse account
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10.0;
+		}
+	}
+
+}
