3f78a092c14a09d18bac1e79c44280003a6453b8
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-04-11 15:03:33 -0300

    classes e metodos abstratos - exercicio

---
+++ src/application/Exercicio3.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+import entities.enums.Color;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		List<Shape> list = new ArrayList<>();
+
+		System.out.print("Enter the number of shapes: ");
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println("Shape #" + (i + 1) + " data:");
+			System.out.print("Rectangle or Circle (r/c)? ");
+			char option = sc.next().charAt(0);
+			System.out.print("Color (BLACK/BLUE/RED): ");
+			Color color = Color.valueOf(sc.next());
+			sc.nextLine();
+			if (option == 'r') {
+				System.out.print("Width: ");
+				double width = sc.nextDouble();
+				System.out.print("Height: ");
+				double height = sc.nextDouble();
+				list.add(new Rectangle(color, width, height));
+			} else {
+				System.out.print("Radius: ");
+				double radius = sc.nextDouble();
+				list.add(new Circle(color, radius));
+			}
+		}
+
+		System.out.println("\nShape Areas:");
+
+		for (Shape obj : list) {
+			System.out.println(obj.Area());
+		}
+
+		sc.close();
+	}
+
+}
